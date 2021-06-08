823c1bd25da45c648c9373cfe609da307194929d
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-06-08 17:33:34 +0300

    snake.py: format, define press at gloibal scope for use of on_release()

--- snake.py
+++ snake.py
@@ -6,48 +6,60 @@ import math as ma
 import time
 import random as ra
 
+
+press = None
+
+
 def on_release(key):
     global releas
     try:
         releas = key.char
-    except AttributeError:pass
+    except AttributeError:
+        pass
     if key:
         return False
+
 
 def on_press(key):
     global press
     try:
         press = key.char
-    except AttributeError:pass
+    except AttributeError:
+        pass
 
-def key_data(q1,q2):
+
+def key_data(q1, q2):
     while 1:
         pres, relea = "a", "z"
-        with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+        with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
             listener.join()
-            #if press == "ф" or press == "ц" or press == "ы" or press == "в":
+            # if press == "ф" or press == "ц" or press == "ы" or press == "в":
             #q1.value = bytes(press, encoding = 'utf-8')
-            print(pres,"press")
+            print(pres, "press")
+
 
 def nothing(x):
     pass
 
-def move(arr,val):
+
+def move(arr, val):
     ste = -1
     arr.append(val)
     arr = arr[ste:] + arr[:ste]
-    #arr[1]=val
+    # arr[1]=val
     return arr
 
-def move2(arr,val):
+
+def move2(arr, val):
     del arr[0]
     arr.append(val)
     return arr
 
-def crt_apl(bad_z,n):
+
+def crt_apl(bad_z, n):
     b = True
     while 1:
-        apl_p = [ra.randint(0,n-1),ra.randint(0,n-1)]
+        apl_p = [ra.randint(0, n-1), ra.randint(0, n-1)]
         for i in bad_z:
             if apl_p == i:
                 b = False
@@ -55,13 +67,19 @@ def crt_apl(bad_z,n):
             return apl_p
             break
 
-def rand_st(pos,n,path,lo,t):
-    time.sleep(t)
-    press = ra.choice([["ф","lef"],["в","rig"],["ц","up"],["ы","dow"]])#randint()
-    print(press[1])
-    return check(press[0],pos,n,path,lo)
 
-def che_in(a,path,ch,pos,p1,p2,m):
+def rand_st(pos, n, path, lo, t):
+    time.sleep(t)
+    press = ra.choice(
+        [["ф", "lef"],
+         ["в", "rig"],
+         ["ц", "up"],
+         ["ы", "dow"]])  # randint()
+    print(press[1])
+    return check(press[0], pos, n, path, lo)
+
+
+def che_in(a, path, ch, pos, p1, p2, m):
     for i in path:
         if i[0] == pos[0]+p1*m and i[1] == pos[1]+p2*m:
             ch = False
@@ -69,11 +87,12 @@ def che_in(a,path,ch,pos,p1,p2,m):
         pos[a] += m
     return pos, ch
 
-def check(press,pos,n,path,lo,rev = False):
-    a1,a2 = 0,1
+
+def check(press, pos, n, path, lo, rev=False):
+    a1, a2 = 0, 1
     if rev == True:
-        a1,a2 = 1,0
-    ch=True
+        a1, a2 = 1, 0
+    ch = True
     if len(path) == 1:
         if press == "ф" and pos[a1] != 0:
             pos[a1] -= 1
@@ -85,23 +104,24 @@ def check(press,pos,n,path,lo,rev = False):
             pos[a2] += 1
     else:
         if press == "ф" and pos[a1] != 0:
-            pos, ch = che_in(a1,path,ch,pos,lo[0],0,-1)
+            pos, ch = che_in(a1, path, ch, pos, lo[0], 0, -1)
         elif press == "в" and pos[a1] != n-1:
-            pos, ch = che_in(a1,path,ch,pos,lo[2],0,1)
+            pos, ch = che_in(a1, path, ch, pos, lo[2], 0, 1)
         elif press == "ц" and pos[a2] != 0:
-            pos, ch = che_in(a2,path,ch,pos,0,lo[1],-1)
+            pos, ch = che_in(a2, path, ch, pos, 0, lo[1], -1)
         elif press == "ы" and pos[a2] != n-1:
-            pos, ch = che_in(a2,path,ch,pos,0,lo[3],1)
+            pos, ch = che_in(a2, path, ch, pos, 0, lo[3], 1)
 
         if pos == path[-1]:
             ch = False
     return pos, ch
 
-apl_col = (0,255,0,255)
-my_col = (255,0,0,255)
-bod_col = (255,0,255,255)
-not_fill = (50,50,50,255)
-sh_col = (0,0,255,100)
+
+apl_col = (0, 255, 0, 255)
+my_col = (255, 0, 0, 255)
+bod_col = (255, 0, 255, 255)
+not_fill = (50, 50, 50, 255)
+sh_col = (0, 0, 255, 100)
 c_s = 48
 ot = 3
 n = 5
@@ -109,35 +129,36 @@ ver = n*(c_s+ot)+ot
 cv.namedWindow("Tracking", cv.WINDOW_NORMAL)
 cv.createTrackbar("len", "Tracking", 0, 25, nothing)
 cv.createTrackbar("use", "Tracking", 0, 1, nothing)
-path = [0,[n//2,n//2]]
-m,c = -1,-1
-pos = [n//2,n//2]
-look = [[pos[0],pos[1]-1],[pos[0]+1,pos[1]],[pos[0],pos[1]+1]]
-look2 = [0,1,1,1]
-apl_p = crt_apl(path,n)
+path = [0, [n//2, n//2]]
+m, c = -1, -1
+pos = [n//2, n//2]
+look = [[pos[0], pos[1]-1], [pos[0]+1, pos[1]], [pos[0], pos[1]+1]]
+look2 = [0, 1, 1, 1]
+apl_p = crt_apl(path, n)
 
 a = np.array([["." for y in range(n)]for x in range(n)])
-a[pos[0]][pos[1]]="#"
+a[pos[0]][pos[1]] = "#"
 
 while 1:
-    pos = [pos[0]%n,pos[1]%n]
+    pos = [pos[0] % n, pos[1] % n]
     if pos == apl_p:
-        #print(path)
-        path = [path[0]]+move(path[1:], path[0])#path[-1])
-        #print(path)
-        apl_p = crt_apl(path[1:],n)
+        # print(path)
+        path = [path[0]]+move(path[1:], path[0])  # path[-1])
+        # print(path)
+        apl_p = crt_apl(path[1:], n)
     else:
         print(path)
 
-    a[apl_p[0]][apl_p[1]]="@"
+    a[apl_p[0]][apl_p[1]] = "@"
 
+    # while 1:
     for _ in range(1):
         #old = a.copy()
-        img = np.zeros((ver,ver, 4), dtype = "uint8")
+        img = np.zeros((ver, ver, 4), dtype="uint8")
         for y in range(n):
             for x in range(n):
-                xy1=((x+1)*c_s+ot*(x+1), (y+1)*c_s+ot*(y+1))
-                xy2=(x*c_s+ot*(x+1), y*c_s+ot*(y+1))
+                xy1 = ((x+1)*c_s+ot*(x+1), (y+1)*c_s+ot*(y+1))
+                xy2 = (x*c_s+ot*(x+1), y*c_s+ot*(y+1))
                 if a[x][y] == "#":
                     cv.rectangle(img, xy1, xy2, my_col, -1)
                 elif a[x][y] == "0":
@@ -166,36 +187,36 @@ while 1:
     #pos, mo = rand_st(pos,n,path[1:],look2,0.00)
 
     for _ in range(1):
-        with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+        with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
             listener.join()
             print(press)
-            pos, mo = check(press,pos,n,path[1:],look2)
+            pos, mo = check(press, pos, n, path[1:], look2)
 
     if mo == True:
-        path = move2(path,pos)
+        path = move2(path, pos)
 
-    if path[0]!=0:
-        dif = (pos[0]-path[-2][0],pos[1]-path[-2][1])
+    if path[0] != 0:
+        dif = (pos[0]-path[-2][0], pos[1]-path[-2][1])
         if dif[0] > 0:
-            look = [[pos[0],pos[1]-1],[pos[0]+1,pos[1]],[pos[0],pos[1]+1]]#print("rig")
-            look2 = [0,1,1,1]
+            look = [[pos[0], pos[1]-1], [pos[0]+1, pos[1]],
+                    [pos[0], pos[1]+1]]  # print("rig")
+            look2 = [0, 1, 1, 1]
         elif dif[0] < 0:
-            look = [[pos[0],pos[1]+1],[pos[0]-1,pos[1]],[pos[0],pos[1]-1]]#print("lef")
-            look2 = [1,1,0,1]
+            look = [[pos[0], pos[1]+1], [pos[0]-1, pos[1]],
+                    [pos[0], pos[1]-1]]  # print("lef")
+            look2 = [1, 1, 0, 1]
         elif dif[1] > 0:
-            look = [[pos[0]+1,pos[1]],[pos[0],pos[1]+1],[pos[0]-1,pos[1]]]#print("down")
-            look2 = [1,0,1,1]
+            look = [[pos[0]+1, pos[1]], [pos[0], pos[1]+1],
+                    [pos[0]-1, pos[1]]]  # print("down")
+            look2 = [1, 0, 1, 1]
         elif dif[1] < 0:
-            look = [[pos[0]-1,pos[1]],[pos[0],pos[1]-1],[pos[0]+1,pos[1]]]#print("up")
-            look2 = [1,1,1,0]
+            look = [[pos[0]-1, pos[1]], [pos[0], pos[1]-1],
+                    [pos[0]+1, pos[1]]]  # print("up")
+            look2 = [1, 1, 1, 0]
             pass
 
-    pos = [pos[0]%n,pos[1]%n]
-    a[pos[0]][pos[1]]="#"
+    pos = [pos[0] % n, pos[1] % n]
+    a[pos[0]][pos[1]] = "#"
     for i in path[1:-1]:
-        if path[0]!=0:
-            a[i[0]][i[1]]="0"
-
-    
-    
-
+        if path[0] != 0:
+            a[i[0]][i[1]] = "0"
